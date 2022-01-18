d3c4d7225a1333cca6a8102d34f2f22cc5a2468d
minhducqn1996 <minhducqn1996@gmail.com>
2022-01-19 00:57:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './globalStyles';
+import { Switch, Route, useLocation } from 'react-router';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from './components/Themes';
+
+import Main from './components/Main';
+import About from './components/AboutPage';
+import Blog from './components/BlogPage';
+import MySkill from './components/MySkillPage';
+import Work from './components/WorkPage';
+
+
+import { AnimatePresence } from "framer-motion";
+import SoundBar from './subComponents/SoundBar';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const location = useLocation();
+
+
+  return <div>
+      <GlobalStyles />
+      <ThemeProvider theme={lightTheme}>
+        <SoundBar />
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/blog' component={Blog} />
+            <Route exact path='/work' component={Work} />
+            <Route exact path='/skills' component={MySkill} />
+          </Switch>
+        </AnimatePresence>
+      </ThemeProvider>
     </div>
-  );
+    
 }
 
-export default App;
+export default App
+
