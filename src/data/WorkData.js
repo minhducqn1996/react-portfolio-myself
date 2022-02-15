ac245ea2e69e4c56f2ed5e045c76bbced9f57592
minhducqn1996 <minhducqn1996@gmail.com>
2022-02-15 19:02:56 +0700

    change data

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,61 +1,30 @@
 export const Work = [
     {
         id:1,
-        name:"Agency Landing Page",
-        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
-        tags:["react","gsap","styledComponents"],
+        name:"Manh Cuong website development",
+        description:"Programming a sales website about Steel, clearly showing the right products for buyers. Website is clear and logical layout.",
+        tags:["HTML5","CSS3","Bootrap3"],
         
-        demo:"https://agency-website-eta.vercel.app/",
-        github:"https://github.com/codebucks27/Agency-website"
+        demo:"http://minhducqn1996.github.io/ManhCuongSteel/",
+        github:"https://github.com/minhducqn1996/ManhCuongSteel"
     },
     {
         id:2,
-        name:"Job listing website",
-        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
-        tags:["react","sass"],
+        name:"Led Thai Duong Website",
+        description:"Developed a website selling interior lighting. Show all models and types of lights. Clearly expressed in accordance with the needs of the buyer.",
+        tags:["HTML5","CSS3", "javascript"],
         
-        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/react-job-listing-website"
+        demo:"https://minhducqn1996.github.io/LedThaiDuong.github.io/",
+        github:"https://github.com/minhducqn1996/LedThaiDuong.github.io"
         
     },
     {
         id:3,
         name:"Dev.to Clone",
-        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-        tags:["react","api","sass"],
+        description:"Developed a responsive single page application allowing users to create an account, book movies, receive tickets, read information about movies, news and more...",
+        tags:["ReactJS","React Context API","Styled Component"],
         
-        demo:"http://devto-clone-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/devto-clone"
+        demo:"https://react-movies-duc.vercel.app/",
+        github:"https://github.com/minhducqn1996/react-portfolio-myself"
     },
-    {
-        id:4,
-        name:"Sidebar Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-        tags:["react","styledComponents","react-router"],
-        
-        demo:"https://react-sidebar.vercel.app/",
-        github:"https://github.com/codebucks27/react-sidebar"
-       
-    },{
-        id:5,
-        name:"Todo App(React + Redux)",
-        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-        tags:["react","redux","framer-motion"],
-        
-        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
 ] 
